6e06c84a2651e9fe9d401bb7999b7e23e7308bf0
Elliott Hughes <enh@google.com>
2014-12-12 12:46:38 -0800

    Add pipe2 and O_CLOEXEC to the libcore POSIX API.
    @hide right now, but we should expose these at some point.
    
    Bug: 18719692
    Change-Id: I0a73606cf089ed307d1ab2916a7df28b3c45aaf0
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- luni/src/main/java/java/nio/SelectorImpl.java
+++ luni/src/main/java/java/nio/SelectorImpl.java
@@ -93,7 +93,7 @@ final class SelectorImpl extends AbstractSelector {
          * configure the pipe so we can fully drain it without blocking.
          */
         try {
-            FileDescriptor[] pipeFds = Libcore.os.pipe();
+            FileDescriptor[] pipeFds = Libcore.os.pipe2(0);
             wakeupIn = pipeFds[0];
             wakeupOut = pipeFds[1];
             IoUtils.setBlocking(wakeupIn, false);
