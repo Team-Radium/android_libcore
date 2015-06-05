9a0fd78c83a0ff1217497cef7fc096ceec23c1ad
W4TCH0UT <ateekujjawal@gmail.com>
2015-06-05 21:17:44 +0530

    Revert "Make ReferenceQueueTest#test_removeJ more robust."
    This reverts commit dfc1c29eefda80ae9d6554eec85f22279e360d7b.

--- harmony-tests/src/test/java/org/apache/harmony/tests/java/lang/ref/ReferenceQueueTest.java
+++ harmony-tests/src/test/java/org/apache/harmony/tests/java/lang/ref/ReferenceQueueTest.java
@@ -22,7 +22,6 @@ import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
-import java.util.concurrent.CountDownLatch;
 import libcore.java.lang.ref.FinalizationTester;
 
 public class ReferenceQueueTest extends junit.framework.TestCase {
@@ -199,29 +198,27 @@ public class ReferenceQueueTest extends junit.framework.TestCase {
         assertNull(rq.poll());
 
         class RemoveThread extends Thread {
-            public final CountDownLatch inBlock = new CountDownLatch(1);
-            public final CountDownLatch outOfBlock = new CountDownLatch(1);
             public void run() {
                 try {
-                    inBlock.countDown();
                     rq.remove(1000L);
                 } catch(InterruptedException ie) {
                     isThrown = true;
                 }
-                outOfBlock.countDown();
             }
         }
         RemoveThread rt = new RemoveThread();
         rt.start();
         try {
-            rt.inBlock.await();
-            // Try to be inside of rq.remove(1000L) if possible.
             Thread.sleep(10);
-        } catch(InterruptedException ie) {}
+        } catch(InterruptedException ie) {
+
+        }
         rt.interrupt();
         try {
-            rt.outOfBlock.await();
-        } catch(InterruptedException ie) {}
+            Thread.sleep(10);
+        } catch(InterruptedException ie) {
+
+        }
         assertTrue(isThrown);
         assertNull(rq.poll());
 
